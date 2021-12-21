25aebed75d67591581ae8bca0b6efc4650806ef9
nm <negyvosiosmergina@icloud.com>
2021-12-21 23:59:13 +0700

    first

---
+++ Prog.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LAB_4_1_POGI_S3
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 0; int b = 0;
+        start:
+            Console.WriteLine("Выберите пункт меню \n    1)Введите A\n    2)Введите B\n    3)Операция +\n    4)Операция -\n    5)Операция *\n    6)Операция /\n");
+            string q = Console.ReadLine();
+
+
+            switch (q)
+            {
+
+
+            }
+        }
+    }
+}
