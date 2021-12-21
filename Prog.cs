65eb4e60a9fa63d58641020d8892ef48f328652c
Danya <danil.sitnikov2000@yandex.ru>
2021-12-22 01:38:28 +0800

    Add case 3

--- Prog.cs
+++ Prog.cs
@@ -19,7 +19,11 @@ namespace LAB_4_1_POGI_S3
 
             switch (q)
             {
-
+                case "3":
+                    Console.Write("\n");
+                    Console.Write("A+B = {0}", a + b);
+                    Console.WriteLine();
+                    goto start;
 
             }
         }
